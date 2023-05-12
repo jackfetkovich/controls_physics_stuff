3b4b6b8d8c12562cee187cf2fe86373c5f67c740
jackfetkovich <johnfetkovich1014@gmail.com>
2023-05-12 18:34:53 -0400

    move motion profile to class

---
+++ main.py
@@ -0,0 +1,7 @@
+from motion_profile import MotionProfile
+import matplotlib.pyplot as plt
+import numpy as np
+
+profile = MotionProfile(10, 4, 200)
+
+
---
+++ motion_profile.py
@@ -0,0 +1,30 @@
+import numpy as np
+import math
+
+class MotionProfile:
+  def __init__(self, max_vel, max_accel, final_pos, current_pos=0):
+    time_to_max_vel = (max_vel / max_accel) * 1000
+    accel_dist = max_accel * (time_to_max_vel/1000)**2
+    remaining_dist = final_pos - current_pos - accel_dist
+    time_in_max_vel = (remaining_dist / max_vel) * 1000
+    
+    if 2*accel_dist > final_pos - current_pos:
+      time_to_max_vel = math.sqrt(2 * accel_dist / max_accel) * 1000
+      time_in_max_vel = 0
+
+    profile_time = 2 * time_to_max_vel + time_in_max_vel
+
+    self.pos = np.zeros(round(profile_time), dtype="float")
+    self.vels = np.zeros(round(profile_time), dtype="float")
+    self.accels = np.zeros(round(profile_time),dtype="float")
+    self.duration = profile_time
+
+    for time in range(round(profile_time)):
+      if time < time_to_max_vel:
+        self.accels[time] = max_accel * (abs(final_pos - current_pos) / (final_pos - current_pos))
+      elif time > time_in_max_vel + time_to_max_vel:
+        self.accels[time] = -max_accel* (abs(final_pos - current_pos) / (final_pos - current_pos))
+      else:
+        self.accels[time] = 0
+      self.vels[time] = self.vels[time - 1] + self.accels[time]/1000
+      self.pos[time] = self.pos[time - 1] + self.vels[time]/1000
