8bdc170e70bfed48a754c40692957261cede701c
jackfetkovich <johnfetkovich1014@gmail.com>
2023-05-19 00:46:26 -0400

    minor changes

--- simulation/main.py
+++ simulation/main.py
@@ -1,34 +1,41 @@
+import sys
+sys.path.append('/home/jack/Documents/control_sim_stuff')
+
 from motion_profile import MotionProfile
+import math
 import time
 import pygame
 import matplotlib.pyplot as plt
 import numpy as np
 
-profile = MotionProfile(60, 22, 400)
+profile = MotionProfile(65, 30, 200)
 
 clock = pygame.time.Clock()
 dt = 0
 
-x_buffer = 0
+start = 0
+x_goal = 0
+x_cur = start
+last = 0
+
 pygame.init()
 screen = pygame.display.set_mode((1700, 600))
-robot = pygame.Rect((200, 280, 40, 40))
+robot = pygame.Rect((start, 280, 40, 40))
 time = 0
-
 
 running = True
 while running:
   screen.fill((0,0,0))
   pygame.draw.rect(screen, (0,255,0), robot)
-  pygame.draw.line(screen, (255,0,0),(200,280), (600,280), 3)
+  pygame.draw.line(screen, (255,0,0),(start,280), (800 + 40,280), 3)
   time += dt
-  x_buffer += profile.get_vel(time) * dt
-  print(x_buffer)
-  dx = x_buffer - x_buffer % 1 if x_buffer > 1 else 0
-  x_buffer = x_buffer if dx == 0 else x_buffer - x_buffer % 1
-  print(dx)
 
-  robot.move_ip(dx, 0)
+  # 1px = 2 distance units
+  x_goal = profile.get_pos(time)
+  x_cur = round(float(x_goal)/0.5) + start
+  robot.move_ip(2*(x_cur - last), 0)
+  print(x_cur)
+  last = x_cur
 
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
